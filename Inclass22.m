0b59cc69776257d1f52fecf5cafbf18795757e23
banglajihwan <31257591+banglajihwan@users.noreply.github.com>
2017-11-16 15:27:03 -0600

    Add files via upload

--- Inclass22.m
+++ Inclass22.m
@@ -5,6 +5,48 @@
 %activated transcription rate for three cases - in which 1, 4, or 8 copies of the
 %transcripton factor are necessary to activate transciption. Comment on your
 %results. 
+figure; hold on; 
+ku = 0; 
+for kb = 0:0.05:5
+    polycoeff = [1, 1-kb, ku] 
+    rts = roots(polycoeff); 
+    rts = rts(imag(rts)==0); 
+    plot(kb*ones(length(rts),1),rts,'r.');
+end 
+title('n =1')
+
+figure; hold on;  
+ku = 0; 
+for kb = 0:0.05:5
+    polycoeff = [1, -kb,0,0,1, ku] 
+    rts = roots(polycoeff); 
+    rts = rts(imag(rts)==0); 
+    plot(kb*ones(length(rts),1),rts,'r.');
+end 
+title('n =4')
+hold off 
+
+figure; hold on;  
+ku = 0; 
+for kb = 0:0.05:5
+    polycoeff = [1, -kb,0,0,0,0,0,0,1, ku] 
+    rts = roots(polycoeff); 
+    rts = rts(imag(rts)==0); 
+    plot(kb*ones(length(rts),1),rts,'r.');
+end 
+title('n =8')
+hold off 
+
+% n = 1 cannot produce two steady states. higher can. 
 
 % 2. Make a similar diagram for the case of an autorepressing gene in the
 % case that 2 copies are need to turn off the gene. 
+figure; hold on; 
+kb = 0; 
+for ku = 0:0.05:5
+    polycoeff = [1, -ku, +1, -kb] 
+    rts = roots(polycoeff); 
+    rts = rts(imag(rts)==0); 
+    plot(ku*ones(length(rts),1),rts,'r.');
+end 
+title('n =2')
